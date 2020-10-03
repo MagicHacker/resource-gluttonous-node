296727c0e5ab183b47d75dbc458499a910ecf29c
Johnson <zxmagichacker@gmail.com>
2020-10-03 18:39:24 +0800

    feat: 引入eslint-config-prettier和eslint-plugin-prettier，并重写eslintrc配置文件

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'alloy',
-    'plugin:@typescript-eslint/recommended',
-    'alloy/typescript',
-  ],
-}
