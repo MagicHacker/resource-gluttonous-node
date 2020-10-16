91078b89de5f9714529be72bf1b6d982c72598b6
Johnson <zxmagichacker@gmail.com>
2020-10-17 00:22:50 +0800

    feat: 添加sequelize的数据库连接测试

---
+++ src/testConnection.ts
@@ -0,0 +1,24 @@
+/**
+ * 测试sequelize连接数据库
+ */
+import { Sequelize } from 'sequelize'
+import dbConfig from './config/db'
+const sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: 'mysql'
+  }
+)
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+testConnection()
