c3365b1674380cf8bd532325173a4d75dd6cf90e
Johnson <zxmagichacker@gmail.com>
2020-10-17 21:50:18 +0800

    feat: 引入常量文件

---
+++ src/config/constant.ts
@@ -0,0 +1,8 @@
+/**
+ * 放置项目的常量
+ */
+
+const port = 3000
+export default {
+  port
+}
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import express from 'express'
 import path from 'path'
 import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index'
 import usersRouter from './routes/users'
-
+import constant from './config/constant'
 const app = express()
 
 app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-app.listen(3000)
-module.exports = app
+app.listen(constant.port)
